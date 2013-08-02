9f13d43cced2b4154d4c737679c7b27de104a469
Andrew Cook <ariscop@gmail.com>
2013-08-02 14:29:00 +1000

    some changes i forgot to commit

--- php_fcgx.c
+++ php_fcgx.c
@@ -3,11 +3,11 @@
 #include "config.h"
 #endif
 
-#include "php.h"
+#include <php.h>
 #include "php_fcgx.h"
 
 static zend_function_entry fcgx_functions[] = {
-    PHP_FE(FCGX_Init, NULL)
+    PHP_FE(FCGX_Listen, NULL)
     PHP_FE(FCGX_Accept, NULL)
     PHP_FE(FCGX_Finish, NULL)
     PHP_FE(FCGX_Hello_World, NULL)
@@ -35,9 +35,28 @@ zend_module_entry fcgx_module_entry = {
 ZEND_GET_MODULE(fcgx)
 #endif
 
-/* ==== Function definitions ==== */
-
 #include <fcgiapp.h>
+
+/**
+ * API:
+ * FCGX_Listen("address"): listen on this address instead of stdio
+ * FCGX_Accept: Accept a request
+ * 
+ * Two options:
+ * return an object from FCGX_Accept:
+ * ->in
+ * ->out
+ * ->err : streams for in/out/err, destroyed when finished
+ * ->env : environment from fcgi
+ * ->finish : finishes the request
+ * 
+ * Second option:
+ * same as above but keep functions seperate from the object
+ * 
+ */
+
+
+/* ==== Stream Opperatins ==== */
 
 size_t php_fcgx_write(php_stream *stream, const char *buf, size_t count TSRMLS_DC);
 size_t php_fcgx_read (php_stream *stream, char *buf,       size_t count TSRMLS_DC);
@@ -54,26 +73,6 @@ PHPAPI php_stream_ops php_fcgx_stream_ops = {
 	.stat  = NULL,
 	.set_option = NULL
 };
-
-/**
- * API:
- * FCGX_Init("address"): initilize and listen on a port/socket
- * FCGX_Accept: Accept a request
- * 
- * Two options:
- * return an object from FCGX_Accept:
- * ->in
- * ->out
- * ->err : streams for in/out/err, destroyed when finished
- * ->env : environment from fcgi
- * ->flush(?) : flushes output streams
- * ->finish : finishes the request
- * 
- * Second option:
- * same as above but keep functions seperate from the object
- * 
- */
-
 
 size_t php_fcgx_write(php_stream *_stream, const char *buf, size_t count TSRMLS_DC)
 {
@@ -121,60 +120,108 @@ static php_stream *php_stream_from_fcgx(FCGX_Stream *stream, const char *mode)
 	return php_stream_alloc_rel(&php_fcgx_stream_ops, stream, NULL, mode);
 }
 
-struct php_fcgx_private {
+/* ==== End Stream Opperatins ==== */
+
+/* ==== Class Definition ==== */
+ZEND_BEGIN_ARG_INFO(arginfo_null, 0)
+ZEND_END_ARG_INFO()
+
+zend_class_entry *fcgx_entry;
+
+typedef struct _FCGXReq {
+	zval *in, *out, *err, *env;
 	FCGX_Request request;
-	zval   *in;
-	zval   *out;
-	zval   *err;
-	int running;
-};
+} FCGXReq;
 
-struct php_fcgx_private priv;
-
-PHP_MINIT_FUNCTION(fcgx) {
-	FCGX_Init();
+PHP_METHOD(FCGXRequest, finish) {
+	FCGX_Finish_r(&priv.request);
 }
 
-PHP_FUNCTION(FCGX_Init)
+PHP_METHOD(FCGXRequest, helloWorld) {
+{
+	zval *object = getThis():
+	FCGX_FPrintF(priv.request.out, "HTTP 200 Ok\r\nContent-type: text/html\r\n\r\n <h1>Hello World!</h1>");
+}
+
+//private null constructor to prevent instantiation?
+PHP_METHOD(FCGXRequest, __construct) {}
+zend_function_entry fcgx_methods[] = {
+	PHP_ME(FCGXRequest, __construct, arginfo_null, ZEND_ACC_PRIVATE)
+	{NULL, NULL, NULL}
+};
+
+/* ==== End Class Definition ==== */
+
+/* ==== Module initilization ==== */
+PHP_MINIT_FUNCTION(fcgx) {
+	FCGX_Init();
+	
+	zend_class_entry ce;
+	INIT_CLASS_ENTRY(ce, "-FCGXRequest", fcgx_methods);
+	fcgx_entry = zend_register_internal_class(&ce TSRMLS_CC);
+	
+	return SUCCESS;
+}
+
+/* ==== EndModule initilization ==== */
+
+/* ==== Global Gunctions ==== */
+int listenfd = -1;
+
+PHP_FUNCTION(FCGX_Listen)
 {
 	char *sock;
 	int   sock_len;
-	int argc = ZEND_NUM_ARGS();
+	int   argc = ZEND_NUM_ARGS();
 	
 	if (zend_parse_parameters(argc TSRMLS_CC, "s", &sock, &sock_len)
 			== FAILURE) {
 		RETURN_BOOL(0);
 	}
 	
+	//currently cant re-open a socket
+	if(listenfd != -1)
+		RETURN_BOOL(0);
+	
 	//make sure its null terminated
 	sock[sock_len] = 0;
 	
-	printf("Listening on %s\n", sock);
+	//printf("Listening on %s\n", sock);
 
 	int sockfd = FCGX_OpenSocket(sock, 1024);
-	//FCGX_Init();
-	int ret = FCGX_InitRequest(&priv.request, sockfd, 0);
-	RETURN_BOOL(ret == 0);
+	if(sockfd != -1) {
+		listenfd = sockfd;
+	}
+	
+	RETURN_BOOL(sockfd != -1);
 }
 
 PHP_FUNCTION(FCGX_Accept)
 {
 	int i, ret;
+	FCGXReq *req = emalloc(sizeof(FCGXReq));
 	
-	ret = FCGX_Accept_r(&priv.request);
-	if(ret != 0)
+	ret = FCGX_InitRequest(req->request, listenfd, 0);
+	if(ret != 0) {
+		printf("Failed to init request");
 		RETURN_NULL();
+	}
+	
+	ret = FCGX_Accept_r(req->request);
+	if(ret != 0) {
+		printf("Failed on accept");
+		RETURN_NULL();
+	}
 	
 	php_stream *_in, *_out, *_err;
-	zval *in, *out, *err, *arr;
 	
-	MAKE_STD_ZVAL(arr);
-	array_init(arr);
-	for(i = 0; priv.request.envp[i] != NULL; i++) {
+	MAKE_STD_ZVAL(req->env);
+	array_init(req->env);
+	for(i = 0; req->request.envp[i] != NULL; i++) {
 		char tmp[4096], *key, *val;
 		tmp[4095] = 0;
 		
-		strncpy(tmp, priv.request.envp[i], 4095);
+		strncpy(tmp, req->request.envp[i], 4095);
 		
 		key = tmp;
 		val = strchr(tmp, '=');
@@ -186,39 +233,30 @@ PHP_FUNCTION(FCGX_Accept)
 		
 		//printf("ENV: %s => %s :: %s\n", key, val, request.envp[i]);
 		
-		add_assoc_string(arr, key, val, 1);
+		add_assoc_string(req->env, key, val, 1);
 	}
 	
-	_in  = php_stream_from_fcgx(priv.request.in,  "r");
-	_out = php_stream_from_fcgx(priv.request.out, "w");
-	_err = php_stream_from_fcgx(priv.request.err, "w");
+	_in  = php_stream_from_fcgx(req->request.in,  "r");
+	_out = php_stream_from_fcgx(req->request.out, "w");
+	_err = php_stream_from_fcgx(req->request.err, "w");
 	
-	MAKE_STD_ZVAL(in);
-	MAKE_STD_ZVAL(out);
-	MAKE_STD_ZVAL(err);
+	MAKE_STD_ZVAL(req->in);
+	MAKE_STD_ZVAL(req->out);
+	MAKE_STD_ZVAL(req->err);
 	
-	php_stream_to_zval(_in,  in);
-	php_stream_to_zval(_out, out);
-	php_stream_to_zval(_err, err);
-	
+	php_stream_to_zval(_in,  req->in);
+	php_stream_to_zval(_out, req->out);
+	php_stream_to_zval(_err, req->err);
 	
 	array_init(return_value);
-	add_assoc_zval(return_value, "in",  in);
-	add_assoc_zval(return_value, "out", out);
-	add_assoc_zval(return_value, "err", err);
-	add_assoc_zval(return_value, "env", arr);
+	add_assoc_zval(return_value, "in",  req->in);
+	add_assoc_zval(return_value, "out", req->out);
+	add_assoc_zval(return_value, "err", req->err);
+	add_assoc_zval(return_value, "env", req->arr);
+	
+	object_and_properties_init(return_value, fcgx_entry, Z_ARRVAL_P(return_value));
 	
 	//RETURN_BOOL(ret == 0)
 	//RETURN_ARRAY(arr);
-}
-
-PHP_FUNCTION(FCGX_Finish)
-{
-	FCGX_Finish_r(&priv.request);
-}
-
-PHP_FUNCTION(FCGX_Hello_World)
-{
-	FCGX_FPrintF(priv.request.out, "HTTP 200 Ok\r\nContent-type: text/html\r\n\r\n <h1>Hello World!</h1>");
 }
 
